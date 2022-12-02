f1782cb4807aeed6e6322b7232ac593a5b8308d4
Vamik76 <merts_v@mail.ru>
2022-12-03 04:29:28 +1000

    Lesson2C#

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿Random random = new Random();
+int number = 0;
+number = random.Next(10,100);
+Console.WriteLine(number);
+int a = number % 10;
+int b = number /10;
+if (a>b)
+    Console.WriteLine ("a>b");
+else if (b>a)
+
+    Console.WriteLine ("b>a");
+else if (b==a)
+    Console.WriteLine (b==a);
---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+
+Random random = new Random();
+int number = 0;
+number = random.Next(100,1000);
+Console.WriteLine(number);
+int a = number / 100;
+int b = number % 10;
+
+Console.WriteLine ("двухзначное число "+a+b);
