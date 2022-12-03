752071ddf5207b2eee01cd1f1c7738db6e6410a8
Vamik76 <merts_v@mail.ru>
2022-12-04 04:57:10 +1000

    HW2C#_end3

---
+++ HOMEWORK3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+Console.WriteLine ("Введите первое чиcло");
+int a = Convert.ToInt32 (Console.ReadLine ());
+Console.WriteLine ("Введите второе чиcло");
+int b = Convert.ToInt32 (Console.ReadLine ());
+if (a*a==b)
+    Console.WriteLine(b+" = квадрату "+a);
+else
+    Console.WriteLine(b+" не = квадрату "+a);
+if (b*b==a)
+    Console.WriteLine(a+" = квадрату "+b);
+else
+    Console.WriteLine(a+" не = квадрату "+b);
