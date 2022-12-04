a383250cc15cc69f51bed0bd637438ce01abd91f
Vamik76 <merts_v@mail.ru>
2022-12-05 00:30:01 +1000

    HW2C#_corret2_string

--- HOMEWORK1/Program.cs
+++ HOMEWORK1/Program.cs
@@ -1,11 +1,7 @@
 ﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 Console.WriteLine ("Введите чиcло");
-int a = Convert.ToInt32 (Console.ReadLine ());
-int b = a / 100 % 10;
-if (b==0)
+string a = Console.ReadLine ();
+if (a.Length<=2)
     Console.WriteLine ("Третьей цифры нет");
 else
-    Console.WriteLine ("третья цифра: " +b);
-    
-
-
+    Console.WriteLine ("третья цифра: " +a[2]);
